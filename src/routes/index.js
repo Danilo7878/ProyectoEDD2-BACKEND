a1b9e3bf26a9fa0faa5ce993cdee3e34bb3a1e0f
Danilo7878 <danilosandovalle@gmail.com>
2018-11-24 05:43:43 -0600

    "correcion esquema mensajes"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,41 +1,103 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user');
 const Message = require('../models/message');
 
 //peticiones de usuario
 router.post('/signup', (req, res) => {
-  const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    username: req.body.username,
-    password: req.body.password,
-    nombre: req.body.nombre,
-    apellido: req.body.apellido,
-    puesto: req.body.puesto,
-    correo: req.body.correo
+  User.find({username: req.body.username})
+  .exec()
+  .then(user => {
+      if(user.length >= 1){
+          return res.status(409).json({message: 'nombre de usuario ya existente'});
+      }else{         
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+              if (err) { res.status(500).json({error: err});
+              }else{
+                  const user = new User({
+                      _id: new mongoose.Types.ObjectId(),
+                      username: req.body.username,
+                      password: hash,
+                      nombre: req.body.nombre,
+                      apellido: req.body.apellido,
+                      puesto: req.body.puesto,
+                      correo: req.body.correo
+                  })
+                  user.save().then(result => {
+                      res.status(201).json({
+                          message: 'usuario creado',
+                          user: user
+                      });
+                  })
+                  .catch(err => {
+                      res.status(500).json({
+                          error: err
+                      });
+                  })
+              }
+          })
+      }
+  })
+  .catch(err =>{
+      error: err
   });
-  user
-  .save().then(result => {
-    res.status(201).json({createdUser:user});
+});
+
+router.post('/login',(req,res) =>{
+  User.find({username: req.body.username})
+  .then(user => {
+      if (user.length < 1) {
+          return res.status(401).json({mensaje: 'Usuario o Contraseña no válido'});
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+          if(err) {
+              return res.status(401).json({
+                  mensaje: 'usuario o contraseña no válido'
+              });
+          }
+          if(result){            
+              const token = jwt.sign({
+                  username: user[0].username
+              }, "este-es-un-token",
+              {
+                  expiresIn: "60"
+              });
+              return res.status(200).json({token: token});
+          }
+           res.status(401).json({message: 'usuario o contraseña no válido'});
+      } )
   })
   .catch(err => {
-    res.status(500).json({error:err});
+      res.status(500).json({ error: err});
   });
 });
 
-router.get('/login',(req,res) =>{
-
+router.delete('/deleteCount/:username', (req, res)=>{
+  User.findOneAndDelete({username:req.params.username})
+  .then(doc => {
+    res.status(200).json(doc);})
+  .catch(err =>{
+      res.status(500).json({error:err});
+    }
+  );
 });
 
-router.delete('/deleteCount', (req, res)=>{
-
+router.get("/profile/:username", (req,res) =>{
+  User.findOne({username:req.params.username})
+  .then(doc => {
+    res.status(200).json(doc);})
+  .catch(err =>{
+      res.status(500).json({error:err});
+    }
+  );
 });
 
 router.get("/users", (req,res) =>{
-  User.find
-  .exec()
+  User.find()
   .then(docs => {
     res.status(200).json(docs);
   })
@@ -47,9 +109,8 @@ router.get("/users", (req,res) =>{
 //peticiones de mensajes
 
 router.get("/conversation", (req, res)=>{
-const emisor = req.body.emisor;
-const receptor = req.body.receptor;
-Message.find({$or:[{"emisor":emisor, "receptor":receptor},{"emisor":receptor,"receptor":emisor}]}).exec()
+Message.find()
+.select('emisor receptor mensaje -_id')
 .then(docs =>{
   if (!(docs.length === 0)){
     res.status(200).json(docs);
